6ab66d1bd8fc472bebe26dc20d6c6e43eb0e7889
jasondinh <dinhtq1@gmail.com>
2018-07-24 08:28:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coding Practice
+
+#### Tips
+1. Greedy Approach
+2. Simplify, adapt and solve strategy
+    * Can you solve a simpler version? i.e. if finding the second largest, can you solve for finding the largest?
+3. Break things down into cases.
---
+++ bt_second_largest.js
@@ -0,0 +1,141 @@
+/*
+    Write a function to find the 2nd largest element in a binary search tree.
+*/
+
+
+// FAILS!
+function findSecondLargest(treeRoot) {
+
+    if (!treeRoot) {
+        return null;
+    }
+
+    const nodes = [];
+    nodes.push([treeRoot, null]);
+
+    while(nodes.length) {
+        const nodePair = nodes.pop();
+        const node = nodePair[0];
+        const ancestor = nodePair[1];
+
+        // check if last
+        if (!node.right && !node.left) {
+            // ancestor has right leaf?
+            if (ancestor.right) {
+                return ancestor.value;
+            } else {
+                return node.value;
+            }
+        }
+
+        // traversal continuation
+        if (node.right) {
+            nodes.push([node.right, node]);
+        }
+
+        if (node.left) {
+            nodes.push([node.left, node]);
+        }
+    }
+
+    return null;
+}
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+// Tests
+
+let desc = 'full tree';
+let treeRoot = new BinaryTreeNode(50);
+let leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+let rightNode = treeRoot.insertRight(70);
+rightNode.insertLeft(60);
+rightNode.insertRight(80);
+assertEquals(findSecondLargest(treeRoot), 70, desc);
+
+desc = 'largest has a left child';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+rightNode = treeRoot.insertRight(70);
+rightNode.insertLeft(60);
+assertEquals(findSecondLargest(treeRoot), 60, desc);
+
+desc = 'largest has a left subtree';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+rightNode = treeRoot.insertRight(70);
+leftNode = rightNode.insertLeft(60);
+leftNode.insertRight(65);
+leftNode = leftNode.insertLeft(55);
+leftNode.insertRight(58);
+assertEquals(findSecondLargest(treeRoot), 65, desc);
+
+desc = 'second largest is root node';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(30);
+leftNode.insertLeft(10);
+leftNode.insertRight(40);
+rightNode = treeRoot.insertRight(70);
+assertEquals(findSecondLargest(treeRoot), 50, desc);
+
+desc = 'descending linked list';
+treeRoot = new BinaryTreeNode(50);
+leftNode = treeRoot.insertLeft(40);
+leftNode = leftNode.insertLeft(30);
+leftNode = leftNode.insertLeft(20);
+leftNode = leftNode.insertLeft(10);
+assertEquals(findSecondLargest(treeRoot), 40, desc);
+
+desc = 'ascending linked list';
+treeRoot = new BinaryTreeNode(50);
+rightNode = treeRoot.insertRight(60);
+rightNode = rightNode.insertRight(70);
+rightNode = rightNode.insertRight(80);
+assertEquals(findSecondLargest(treeRoot), 70, desc);
+
+desc = 'one node tree';
+treeRoot = new BinaryTreeNode(50);
+assertThrowsError(() => findSecondLargest(treeRoot), desc);
+
+desc = 'when tree is empty';
+treeRoot = null;
+assertThrowsError(() => findSecondLargest(treeRoot), desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
