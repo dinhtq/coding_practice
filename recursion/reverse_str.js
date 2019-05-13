70ef10c622ef40ed5f4fd03930375796d1722623
Jason Dinh <jdinh@Frederik-NMBP-2.local>
2019-05-13 11:07:07 -0500

    recursion revser str

---
+++ recursion/reverse_str.js
@@ -0,0 +1,9 @@
+function reverse(string) {
+  // bail case
+  if (string.length === 1) return string
+  // process + recurse
+  return reverse(string.slice(1)) + string[0]
+}
+
+console.log(reverse('jason') === 'nosaj')
+console.log(reverse('jason was here') === 'ereh saw nosaj')
