feca29799c336d5be7636d3da43c466305643aeb
Jason Dinh <jdinh@Frederik-NMBP-2.local>
2019-05-22 16:02:49 -0500

    bt inorder recursive

---
+++ trees/bt_inorder_traversal.js
@@ -0,0 +1,62 @@
+// traverse binary tree in order
+// https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
+
+function inorderTraversal(root) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// recursive
+// function inorderTraversal(root, arr) {
+//   if (!root) return
+  
+//   if (root.left) inorderTraversal(root.left, arr)
+  
+//   if (root) {
+//       arr.push(root.val)
+//   }
+  
+//   if (root.right) inorderTraversal(root.right, arr)
+  
+//   return arr
+// }
