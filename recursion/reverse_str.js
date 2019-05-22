d45021a392dc2efde9e0edc571f38078720d95ea
Jason Dinh <jdinh@Frederik-NMBP-2.local>
2019-05-22 15:15:26 -0500

    empty rev str function

--- recursion/reverse_str.js
+++ recursion/reverse_str.js
@@ -1,9 +1,58 @@
-function reverse(string) {
-  // bail case
-  if (string.length === 1) return string
-  // process + recurse
-  return reverse(string.slice(1)) + string[0]
+
+// reverse string recursively
+// jason -> nosaj
+
+function reverse(str) {
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function reverse(string) {
+//   // bail case
+//   if (string.length === 1) return string
+//   // process + recurse
+//   return reverse(string.slice(1)) + string[0]
+// }
 
 console.log(reverse('jason') === 'nosaj')
 console.log(reverse('jason was here') === 'ereh saw nosaj')
