9785d8f718f40aada3d5cffb187a86ecc2159696
jasondinh <dinhtq1@gmail.com>
2019-06-06 19:26:35 -0500

    added rangeSumBST

---
+++ trees/rangeSum.js
@@ -0,0 +1,57 @@
+/*
+  Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+  The binary search tree is guaranteed to have unique values.
+
+  https://leetcode.com/problems/range-sum-of-bst/
+*/
+
+var rangeSumBST = function(root, L, R) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var rangeSumBST = function(root, L, R) {
+//   var stack = [root]
+//   var sum = 0
+  
+//   while(stack.length) {
+//       var node = stack.pop()
+      
+//       if(!node) {
+//           continue
+//       }
+      
+//       if (node.val >= L && node.val <= R) {
+//           sum += node.val
+//       }
+      
+//       if (node.left) stack.push(node.left)
+//       if (node.right) stack.push(node.right)
+//   }
+  
+//   return sum
+// }
