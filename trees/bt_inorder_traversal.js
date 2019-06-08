7ba0b71852cb03279f91ae6ada5199fa0d04bb84
jasondinh <dinhtq1@gmail.com>
2019-06-08 13:42:16 -0500

    added test cases for inorder traversal

--- trees/bt_inorder_traversal.js
+++ trees/bt_inorder_traversal.js
@@ -1,19 +1,87 @@
+
+
 // traverse binary tree in order
 // https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
 
-function inorderTraversal(root) {
+
+
+
+function inorderTraversal(root) {}
+
+
+
+
+
+
+
+
+
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
 }
 
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
 
+function assertEqualsObject(a, b, desc) {
+  if (_.isEqual(a, b)) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
 
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
 
+const _ = require('lodash')
 
+// Tests
+let desc = 'full tree'
+let treeRoot = new BinaryTreeNode(1)
+let firstLeft = treeRoot.insertLeft(2)
+firstLeft.insertLeft(3)
+firstLeft.insertRight(4)
+let firstRight = treeRoot.insertRight(5)
+firstRight.insertLeft(6)
+firstRight.insertRight(7)
+assertEqualsObject(inorderTraversal(treeRoot), [3, 2, 4, 1, 6, 5, 7], desc)
 
+desc = 'Partial tree'
+treeRoot = new BinaryTreeNode(1)
+firstRight = treeRoot.insertRight(2)
+firstRight.insertLeft(3)
+assertEqualsObject(inorderTraversal(treeRoot), [1, 3, 2], desc)
 
-
-
-
-
+desc = 'Empty tree'
+treeRoot = null
+assertEqualsObject(inorderTraversal(treeRoot), [], desc)
 
 
 
@@ -52,11 +120,32 @@ function inorderTraversal(root) {
   
 //   if (root.left) inorderTraversal(root.left, arr)
   
-//   if (root) {
-//       arr.push(root.val)
-//   }
+//   arr.push(root.val)
   
 //   if (root.right) inorderTraversal(root.right, arr)
   
 //   return arr
+// }
+
+// iterative
+// function inorderTraversal(root) {
+//   if (!root) return []
+
+//   const stack = []
+//   const values = []
+
+//   let curNode = root
+
+//   while(curNode || stack.length) {
+//       while (curNode) {
+//           stack.push(curNode)
+//           curNode = curNode.left
+//       }
+
+//       curNode = stack.pop()
+//       values.push(curNode.value)
+//       curNode = curNode.right
+//   }
+
+//   return values
 // }
